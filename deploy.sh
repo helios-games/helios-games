8a1166ac5dd9d9372449ce6f2980190759d6a8c4
Alex Collins <alex.e.c@gmail.com>
2017-02-24 11:42:59 +0000

    force

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 set -eux
 
-for F in \
-  ./deploy/statedb.yml \
-  ./deploy/walletdb.yml \
-  ./wallet/wallet.yml \
-  ./gf/gf.yml \
-  ./web/web.yml \
-  ; do
+eval $(mimikube docker-env)
+
+W=${*:-statedb walletdb wallet gf web}
+
+for U in $W ; do
+  case $U in
+    statedb|walletdb )
+        F=deploy/$U.yml
+      ;;
+    *)
+      docker images |grep alexec/web|awk '{printf "%s:%s", $1, $2}'|xargs docker rmi  --force
+      F=$U/$U.yml
+      ;;
+  esac
   kubectl create -f $F || kubectl replace --force -f $F
 done
