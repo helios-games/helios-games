ed65fcf8bda835aada5a1c4156a3a94ad8adf6fc
Alex Collins <alex.e.c@gmail.com>
2017-03-01 15:05:36 +0000

    circle

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
-set -eux
+set -eu
 
 eval $(minikube docker-env)
 
-W=${*:-statedb walletdb wallet gf web}
-
-for U in $W ; do
+for U in ${*:-statedb walletdb wallet gf web} ; do
+  U=$1
   case $U in
-    statedb|walletdb )
-        F=deploy/$U.yml
-      ;;
-    *)
-      docker images |grep alexec/$U|awk '{printf "%s:%s ", $1, $2}'|xargs docker rmi  --force
-      F=$U/$U.yml
-      ;;
+    statedb|walletdb) F=deploy/$U.yml ;;
+    *) F=$U/$U.yml ;;
   esac
   kubectl create -f $F || kubectl replace --force -f $F
 done
