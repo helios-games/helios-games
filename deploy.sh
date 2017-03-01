89c47f3dafb6e956d848919425d86d1ea966a294
Alex Collins <alex.e.c@gmail.com>
2017-03-01 18:51:30 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 set -eu
 
-eval $(minikube docker-env)
-
 for U in ${*:-statedb walletdb wallet gf web} ; do
-  U=$1
   case $U in
     statedb|walletdb) F=deploy/$U.yml ;;
     *) F=$U/$U.yml ;;
